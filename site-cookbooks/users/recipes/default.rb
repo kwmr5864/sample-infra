86f452c3d1e00fbfd69e9ab5bd47cf2d0c19bb4d
Masayuki Kawamura <kwmr5864@gmail.com>
2016-06-05 10:32:29 +0900

    サンプルのグループとユーザをレシピに追加

--- site-cookbooks/users/recipes/default.rb
+++ site-cookbooks/users/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+group 'sample' do
+  group_name 'sample'
+  gid 999
+  action :create
+end
+
+user 'sample' do
+  comment 'sample user'
+  group 'sample'
+  home '/home/sample'
+  shell '/bin/bash'
+  supports :manage_home => true
+  action: create
+end
+
